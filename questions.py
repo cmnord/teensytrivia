4a683f3fef8892a814bbbff5fe9d19347b9050c8
Claire Nord <cnord@mit.edu>
2016-04-10 16:19:00 -0400

    First commit

---
+++ questions.py
@@ -0,0 +1,4 @@
+#6.S08 FINAL PROJECT
+#April 14th deadline: get working prototype of “questions.py” and do database setup.
+#Pick which tables the database will have and what columns those tables will contain.
+#This will probably require deciding on your “join game” functionality.
