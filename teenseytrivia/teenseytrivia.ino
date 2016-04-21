a51863a51272cabf39944e548104ee8f16598821
Claire Nord <cnord@mit.edu>
2016-04-21 15:26:34 -0400

    Adds buttons and basic response waiting/recording functionality

--- teenseytrivia/teenseytrivia.ino
+++ teenseytrivia/teenseytrivia.ino
@@ -29,10 +29,28 @@ String postOrBroadcast = "post";
 
 ESP8266 wifi = ESP8266(true);  //Change to "true" or nothing for verbose serial output
 
+//define buttons
+int a_button = 4; //black
+int b_button = 2; //black
+int c_button = 8; //yellow
+int d_button = 6; //blue
 int led = 13;
 
 void setup() {
   Serial.begin(115200);
+  //button setup
+  pinMode(a_button, INPUT); //set pin to "listen" to signals
+  pinMode(b_button, INPUT); //set pin to "listen" to signals
+  pinMode(c_button, INPUT); //set pin to "listen" to signals
+  pinMode(d_button, INPUT); //set pin to "listen" to signals
+  pinMode(led, OUTPUT); //set LED to be output
+  pinMode(a_button, INPUT_PULLUP); //set input to be normally "HI"
+  pinMode(b_button, INPUT_PULLUP); //""
+  pinMode(c_button, INPUT_PULLUP); //""
+  pinMode(d_button, INPUT_PULLUP); //""
+  digitalWrite(led, LOW); //Set LED to be off at beginning
+
+  //display setup
   delay(2500);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
@@ -99,24 +117,36 @@ void loop() {
       tLastIotReq = millis();
     }
   }
-
-
   if ((IOT && wifi.hasResponse())) {
     Serial.println("HAVE IOT");
     resp = wifi.getResponse();
+    resp = resp.substring(8, resp.length() - 5);
     tLastIotResp = millis();
     updateDisplay();
-
     Serial.println(resp);
-    delay(1000);//can read for 5 seconds before reset
-
+    while (digitalRead(a_button) &&
+           digitalRead(b_button) &&
+           digitalRead(c_button) &&
+           digitalRead(d_button)) {
+      //delay until you press a button again
+      delay(50);
+    }
+    if (!digitalRead(a_button)) {
+      Serial.println("A");
+    }
+    else if (!digitalRead(b_button)) {
+      Serial.println("B");
+    }
+    else if (!digitalRead(c_button)) {
+      Serial.println("C");
+    }
+    else if (!digitalRead(d_button)) {
+      Serial.println("D");
+    }
   }
-
-  //}
-  delay(50);
 }
-void updateDisplay()
-{
+
+void updateDisplay() {
   Serial.println("HI IM DISPLAING");
   display.clearDisplay();
   display.setCursor(0, 0);
