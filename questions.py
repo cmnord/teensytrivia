1ad5b582525290973096f418af9719e2589d1f95
Claire Nord <cnord@mit.edu>
2016-04-21 16:11:45 -0400

    Re-reformats for parseability

--- questions.py
+++ questions.py
@@ -20,11 +20,18 @@ json_data = json.loads(html)
 p = randint(0,(len(json_data["questions"]))) - 1 #random question index number
 q_id = (json_data["questions"][p]["id"]).strip()
 prompt = json_data["questions"][p]["prompt"]
+
 ans_list = json_data["questions"][p]["ans_list"]
+ans_list = ans_list[1:-1] #cuts off [ and ]
+ans_list = ans_list.split(", ") #turns this into a real python list
+
 correct_ans = json_data["questions"][p]["correct_ans"]
-print("<b>" + q_id + ". " + prompt + "</b>")
-print("<ul><li>" + ans_list + "</li>")
-print("<li>" + correct_ans + "</li></ul>")
+print("<h1>" + q_id + ". " + prompt + "</h1>")
+print("<ul>")
+alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in range(len(ans_list)):
+    print("<li><%s>%s</%s></li>" %(alph[i],ans_list[i],alph[i]))
+print("</ul><h4>" + correct_ans + "</h4>")
 
 
 ########## DO NOT CHANGE ANYTHING BELOW THIS LINE ##########
