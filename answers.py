152c7badd2d2305a0815c0b7c2e26e89a75920ae
jennycxu <jennycxu@mit.edu>
2016-04-26 22:46:31 -0400

    now updates entries + inserts new ones and adds delete functionality

--- answers.py
+++ answers.py
@@ -11,14 +11,6 @@ method_type = get_method_type()
 form = cgi.FieldStorage() #get specified parameters!
 
 if method_type == 'POST':
-    sender = "asdf"
-    deviceType = "asdf"
-    gameID = 0
-    questionID = 1
-    roundNum = 0
-    delta = 0.2
-    isCorrect = False
-    currentScore = 0
     gameID = form.getvalue("gameID")
     roundNum = form.getvalue("roundNum")
     questionID = form.getvalue("questionID")
@@ -30,12 +22,18 @@ if method_type == 'POST':
     
     
     cnx = _mysql.connect(user='cnord_jennycxu', passwd='Pg8rdAyj',db='cnord_jennycxu')
-
-    #create a mySQL query and commit to database relevant information for logging message
-    query = ("INSERT INTO response_db (gameID, roundNum, questionID, sender, deviceType, delta, isCorrect, currentScore) VALUES (" + str(gameID) + ", " + str(roundNum)  + ", " + str(questionID) + ", \'" + str(sender)  + "\', \'" + str(deviceType)  + "\', " + str(delta)  + ", " + str(isCorrect) + ", " + str(currentScore) + ")")
+    query = ("SELECT * FROM response_db WHERE sender=\'"+ str(sender)+"\'")
+    cnx.query(query)
+    result = cnx.store_result()
+    rows = result.fetch_row(maxrows=0,how=0)
+    #if there is some entry already there for someone, just update it instead of making a new entry
+    if(len(rows) > 0):
+        query = ("INSERT INTO response_db (gameID, roundNum, questionID, sender, deviceType, delta, isCorrect, currentScore) VALUES (" + str(gameID) + ", " + str(roundNum)  + ", " + str(questionID) + ", \'" + str(sender)  + "\', \'" + str(deviceType)  + "\', " + str(delta)  + ", " + str(isCorrect) + ", " + str(currentScore) + ")")
+    else:
+        query = ("UPDATE response_db SET currentScore=" + str(currentScore) + ", isCorrect=" + str(isCorrect) + ", delta=" + str(delta) + ",questionID=" + str(questionID) + ",roundNum=" + str(roundNum) + " WHERE sender=\'" + str(sender) +"\'")
     cnx.query(query)
     cnx.commit()
-    #print("<br>Inserted into database success!")
+    #create a mySQL query and commit to database relevant information for logging message
     cnx.close()#close so we don't go over max connections
 elif method_type == 'GET':
    # Now pull data from database and compute on it
@@ -43,10 +41,11 @@ elif method_type == 'GET':
     cnx.query(query)
     result = cnx.store_result()
     rows = result.fetch_row(maxrows=0,how=0) #what does this do?
-    
+    cnx.commit()
     #print leaderboard stuff now
     print('<h1>What is in the Database</h1>')
     print(rows)
+    
     #later: format the results of the query to show the top scorers and their scores
     #also later: make sure this printing works in 2 ways: one for web, one for teensy
 print('</html>')
--- results.py
+++ results.py
@@ -20,20 +20,26 @@ cnx = _mysql.connect(user='cnord_jennycxu', passwd='Pg8rdAyj',db='cnord_jennycxu
 method_type = get_method_type()
 
 form  = get_params()
-#print("form= ")
-#print(form)
 
 if method_type == "GET": #leaderboard
     # Now pull data from database and compute on it
-    shouldDelete = form.getvalue("shouldDelete")[0] == "T"
+    shouldDelete = False
+    if 'shouldDelete' in form.keys():
+        shouldDelete = form.getvalue("shouldDelete")[0] == "T"
     if(shouldDelete):
-        query = ("DELETE FROM response_db *")
+        query = ("DELETE FROM response_db")
         try:
             cnx.query(query)
             cnx.commit()
         except:
            # Rollback in case there is any error
            cnx.rollback()
+        query = ("SELECT * FROM response_db ORDER BY currentScore") #should return senders with 5 highest scores (bug: there will be duplicates from the same game...)
+        cnx.query(query)
+        result = cnx.store_result()
+        rows = result.fetch_row(maxrows=0,how=0) #what does this do?
+        print(rows)
+        
     else:
         query = ("SELECT * FROM response_db ORDER BY currentScore") #should return senders with 5 highest scores (bug: there will be duplicates from the same game...)
         cnx.query(query)
@@ -52,28 +58,6 @@ if method_type == "GET": #leaderboard
             
         #later: format the results of the query to show the top scorers and their scores
         #also later: make sure this printing works in 2 ways: one for web, one for teensy
-
-
-elif method_type == "POST":
-    if "gameID" in form.keys() and "roundNum" in form.keys() and "questionID" in form.keys() and "sender" in form.keys() and "deviceType" in form.keys() and "delta" in form.keys():
-        print("Pushing to Database")
-        gameID = form.getvalue("gameID")
-        roundNum = form.getvalue("roundNum")
-        questionID = form.getvalue("questionID")
-        sender = form.getvalue("sender")
-        deviceType = form.getvalue("deviceType")
-        delta = form.getvalue("delta")
-
-        query = ("SELECT sender FROM response_db WHERE sender=" + sender) #should return senders with 5 highest scores (bug: there will be duplicates from the same game...)
-        cnx.query(query)
-        result = cnx.store_result()
-        rows = result.fetch_row(maxrows=0,how=0) #what does this do?
-        if(len(rows) == 0):# if doesn't exist yet, insert into database
-            query = ("INSERT INTO response_db (gameID, roundNum, questionID, sender, deviceType, delta) VALUES (\'" + gameID + "\', \'" + roundNum  + "\', \'" + questionID + "\', \'" + sender  + "\', \'" + deviceType  + "\', \'" + delta  + "\')")
-            cnx.query(query)
-            cnx.commit()
-        else:
-            print("EXISTS")
         
 
 ########## DO NOT CHANGE ANYTHING BELOW THIS LINE ##########
