a43e161305e3f5f3cee3166fdc36c77e73ba919d
Claire Nord <cnord@mit.edu>
2016-04-28 16:58:15 -0400

    Changes leaderboard format based on deviceType argument.

--- results.py
+++ results.py
@@ -8,10 +8,6 @@ exec(open("/var/www/html/student_code/LIBS/s08libs.py").read())
 # Do some HTML formatting
 print( "Content-type:text/html\r\n\r\n")
 print('<html>')
-print('<head>')
-print('<title>Posts Trivia Responses to Database</title>')
-print('</head>')
-print('<body>')
 
 #Set up the server connection
 cnx = _mysql.connect(user='cnord_jennycxu', passwd='Pg8rdAyj',db='cnord_jennycxu')
@@ -45,25 +41,33 @@ if method_type == "GET": #leaderboard
         cnx.query(query)
         result = cnx.store_result()
         rows = result.fetch_row(maxrows=0,how=0) #what does this do?
-        
-        #print leaderboard stuff now
-        print('<h1>LEADERBOARD</h1>')
-        print('<h2>Your current leaders are:</h2>')
-        playersCount = 0
-        for i in range(len(rows)):
-            if rows[i][8] != None:
-                playersCount = playersCount + 1
-                print('<p>'+ str(playersCount) + "." + rows[i][4].decode("utf-8") + " : " + str(rows[i][8]))
-                print('</p>')
-            
+        if(form.getvalue('deviceType') == 'teensy' or form.getvalue('deviceType') == 'teensey'):
+            print('LEADERBOARD\n')
+            playersCount = 0
+            for i in range(len(rows)):
+                if rows[i][8] != None:
+                    playersCount = playersCount + 1
+                    print(str(playersCount) + "." + rows[i][4].decode("utf-8") + " : " + str(rows[i][8]) + "\n")            
+        else:
+            print('<head>')
+            print('<title>Posts Trivia Responses to Database</title>')
+            print('</head>')
+            print('<body>')
+            #print leaderboard stuff now
+            print('<h1>LEADERBOARD</h1>')
+            print('<h2>Your current leaders are:</h2>')
+            playersCount = 0
+            for i in range(len(rows)):
+                if rows[i][8] != None:
+                    playersCount = playersCount + 1
+                    print('<p>'+ str(playersCount) + "." + rows[i][4].decode("utf-8") + " : " + str(rows[i][8]))
+                    print('</p>')
+            print('</body>')
         #later: format the results of the query to show the top scorers and their scores
         #also later: make sure this printing works in 2 ways: one for web, one for teensy
         
 
 ########## DO NOT CHANGE ANYTHING BELOW THIS LINE ##########
-print('</body>')
 print('</html>')
-
-
 
 
