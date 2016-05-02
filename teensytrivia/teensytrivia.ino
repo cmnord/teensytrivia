f5f1ce2463d39e83414b0612b744a969b136e4a6
Claire Nord <cnord@mit.edu>
2016-05-01 22:08:10 -0400

    Add endgame

--- teensytrivia/teensytrivia.ino
+++ teensytrivia/teensytrivia.ino
@@ -13,7 +13,8 @@ Adafruit_SSD1306 display(4);
 #define IOT_UPDATE_INTERVAL 1000 //snce want to get every 1000 seconds and alternate posting/getting so need 1/2
 #define wifiSerial Serial1          // for ESP chip
 
-String kerberos = "jennycxu";        // UPDATE WITH YOUR KERBEROS
+//wifi/internet globals
+String kerberos = "cnord";        // UPDATE WITH YOUR KERBEROS
 String MAC = "";
 String resp = "";
 uint32_t tLastIotReq = 0;       // time of last send/pull
@@ -21,6 +22,7 @@ uint32_t tLastIotResp = 0;      // time of last response
 String domain = "iesc-s2.mit.edu";
 int port = 80;
 
+//game globals
 uint32_t tQuestionStart = 0; //used for determining delta
 uint32_t tQuestionEnd = 0; //used for determining delta
 float delta = 0; //time it takes for player to answer
@@ -29,6 +31,7 @@ bool isCorrect = 0;
 //int currentScore = 0; //+10 for every correct answer
 int roundNum = 0; //this gets incremented every loop
 int gameID = 0; //this gets changed later
+int MAX_ROUNDS = 5; //game ends after MAX_ROUNDS questions
 
 #define SSID "MIT GUEST" // network SSID and password
 #define PASSWORD ""
@@ -36,10 +39,10 @@ int gameID = 0; //this gets changed later
 ESP8266 wifi = ESP8266(true);  //Change to "true" or nothing for verbose serial output
 
 //define buttons
-int a_button = 11; //black
-int b_button = 12; //black
-int c_button = 10; //yellow
-int d_button = 9; //blue
+int a_button = 4; //black
+int b_button = 2; //black
+int c_button = 8; //yellow
+int d_button = 6; //blue
 
 void setup() {
   Serial.begin(115200);
@@ -83,6 +86,9 @@ void setup() {
 void loop() {
   Serial.println("----------begin loop-----------------");
   wifi.clearRequest();
+  if(roundNum==MAX_ROUNDS){
+    endGame();
+  }
   roundNum += 1;
   //if (millis() - tLastIotReq >= IOT_UPDATE_INTERVAL) {
     resp = getQuestion();
@@ -95,7 +101,7 @@ void loop() {
   //resp is now nicely formatted
   updateDisplay(resp);
   tQuestionStart = millis();
-  while (digitalRead(a_button) && digitalRead(b_button) && digitalRead(c_button) && digitalRead(d_button) ) {
+  while (digitalRead(a_button) && digitalRead(b_button) && digitalRead(c_button) && digitalRead(d_button)) {
     //delay until you press a button again
     delay(50); //this means all deltas will be in 50-ms increments (adjust the delay to make shorter delta)
   }
@@ -155,6 +161,31 @@ void menu() {
   //if they select the A button, the method will just end and loop will begin, which is the game
 }
 
+void endGame() {
+  String menuText = "Game over! Here are your final results:";
+  updateDisplay(menuText);
+  delay(3000);
+  String lead = getLeaderboard();
+  updateDisplay(lead);
+  delay(2000);
+  menuText = "Play again?\nA. YES\nB. NO";
+  updateDisplay(menuText);
+  while (digitalRead(a_button) && digitalRead(b_button)) {
+    //delay until you press A or B
+    delay(50);
+  }
+  if (!digitalRead(a_button)) {
+    roundNum = 0;
+    menu();
+  }
+  if (!digitalRead(b_button)) {
+    menuText = "GAME OVER";
+    updateDisplay(menuText);
+    while(true){delay(9000);} //delay forever
+  }
+  //if they select the A button, the game will restart
+}
+
 String getQuestion() {
   //gets a question from sb1.py.
   String response = "";
@@ -187,7 +218,7 @@ String getLeaderboard() {
   if (wifi.isConnected() && !wifi.isBusy()) {
     Serial.print("Getting leaderboard at t=");
     Serial.println(millis());
-    String getPath = "/student_code/cnord/dev1/sb2.py";
+    String getPath = "/student_code/" + kerberos + "/dev1/sb2.py";
     String getParams = "sender=" + kerberos + "&deviceType=teensy";
     wifi.sendRequest(GET, domain, port, getPath, getParams);
     unsigned long t = millis();
@@ -263,12 +294,13 @@ int parseResponse(String response) {
   int ans_pin = a_button;
   if (correct_ans.equals(ans_b)) {
     ans_pin = b_button;
-  } else if (correct_ans.equals(ans_c)) {
+  }
+  else if (correct_ans.equals(ans_c)) {
     ans_pin = c_button;
-  } else if (correct_ans.equals(ans_d)) {
+  }
+  else if (correct_ans.equals(ans_d)) {
     ans_pin = d_button;
   }
-  
   resp = question + "\n";
   resp += "A. " + ans_a + "\nB. " + ans_b + "\nC. " + ans_c + "\nD. " + ans_d;
   return ans_pin;
